94a8b53c57e645d109cd12b7ad677d3cca0bd3c4
chien <minhc8435@gmail.com>
2025-10-19 16:48:50 +0700

    Thêm controller hahahaha

---
+++ BanDTh/Controllers/hahahahahaha.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BanDTh.Controllers
+{
+    public class hahahahahaha : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
